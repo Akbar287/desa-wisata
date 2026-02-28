0592c1ab0d92ea8dec60354edd7dc63f83b6cc2f
Akbar287 <muhammadakbar007akbar@gmail.com>
2026-02-28 12:48:29 +0700

    fixing zod v3

--- validation/LoginValidation.tsx
+++ validation/LoginValidation.tsx
@@ -1,15 +1,17 @@
-import { z } from "zod"
+import { z } from "zod/v3"
 
 export const LoginSkemaValidation = z.object({
     username: z
         .string({
-            error: (iss) => iss.input === undefined ? "Nama Pengguna diperlukan" : "Nama Pengguna harus memiliki minimal 8 karakter",
+            required_error: "Nama Pengguna diperlukan",
+            invalid_type_error: "Nama Pengguna harus berupa teks",
         })
         .min(8, "Nama Pengguna harus memiliki minimal 8 karakter")
         .max(16, "Nama Pengguna harus memiliki maksimal 16 karakter"),
     password: z
         .string({
-            error: (iss) => iss.input === undefined ? "Kata Sandi diperlukan" : "Kata Sandi harus memiliki minimal 8 karakter",
+            required_error: "Kata Sandi diperlukan",
+            invalid_type_error: "Kata Sandi harus berupa teks",
         })
         .min(8, "Kata Sandi harus memiliki minimal 8 karakter"),
 })

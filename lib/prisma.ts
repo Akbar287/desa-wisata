d3a427a2d8ec7aadf12f6da39825e7857a6bfbe9
Akbar287 <muhammadakbar007akbar@gmail.com>
2026-03-26 23:21:17 +0700

    remove pool pg

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,20 @@
 import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+const prismaClient =
+  global.prisma ??
+  new PrismaClient({
+    adapter: new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    }),
+  });
 
-const prismaClient = global.prisma ?? new PrismaClient({
-  adapter: new PrismaPg(pool),
-});
-
-if (process.env.NODE_ENV !== "production") global.prisma = prismaClient;
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prismaClient;
+}
 
 export const prisma = prismaClient;
